dd991c9f2f0b5a9d48c54c21b72ed27062be4a3e
ESSBBSR\rajalaxmi <rajalaxmi@esspl.com>
2025-10-09 18:32:34 +0530

    String comparison done using when.
    Signed-off-by: ESSBBSR\rajalaxmi <rajalaxmi@esspl.com>

--- src/Main.kt
+++ src/Main.kt
@@ -1,12 +1,16 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-import kotlin.random.Random
-
 fun main() {
 
-    val firstResult = Random.nextInt(6)
-    val secondResult = Random.nextInt(6)
-    println("First result: $firstResult")
-    println("Second result: $secondResult")
-    println(if (firstResult == secondResult) "You win :)" else "You lose :(")
+    println("Type a button name:")
+    val button = readln()
+
+    println(when (button.uppercase()) {
+
+            "A" -> "Yes"
+            "B" -> "No"
+            "X" -> "Menu"
+            "Y" -> "Nothing"
+            else -> "There is no such button"
+        })
 }
