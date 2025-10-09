8a0c0f4e55091301178f42014cee52115c0b7673
ESSBBSR\rajalaxmi <rajalaxmi@esspl.com>
2025-10-09 12:37:45 +0530

    Data type testing done

--- src/Main.kt
+++ src/Main.kt
@@ -1,11 +1,20 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    println("What's your name?")
-    val name = readln()
-    println("What's your age?")
-    val age = readln()
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("$name is $age years old.")
+
+    val a: Int = 1000
+    val log: String = "Some log message"
+    val b: Float = 33.65f
+    val c: Double = 87.12
+    val d: Boolean = true
+    val e: Char = 'q'
+    val f: Long = 1000_000_000_000_000
+
+    println("a: $a")
+    println("log: $log")
+    println("b: $b")
+    println("c: $c")
+    println("d: $d")
+    println("e: $e")
+    println("f: $f")
 }
