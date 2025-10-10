e3c46ca6db0a2489befe72664bd79684d6f857d5
ESSBBSR\rajalaxmi <rajalaxmi@esspl.com>
2025-10-10 13:58:48 +0530

    FizzBuzz game created for 1 to 100
    Signed-off-by: ESSBBSR\rajalaxmi <rajalaxmi@esspl.com>

--- src/Main.kt
+++ src/Main.kt
@@ -2,25 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
 
-    val totalPizzaSlices = 8
+    for (num in 1..100) println(when {
 
-    // Using while
-    var sliceCount = 1
-    while (sliceCount < totalPizzaSlices) {
-
-        println("There is only $sliceCount slice/s of pizza :(")
-        sliceCount++
-    }
-    println("There are $sliceCount slices of pizza. Hooray! We have a whole pizza! :D\n")
-
-    // Using do-while
-    sliceCount = 0
-    do {
-
-        sliceCount++
-        if (sliceCount < totalPizzaSlices)
-            println("There is only $sliceCount slice/s of pizza :(")
-        else
-            println("There are $sliceCount slices of pizza. Hooray! We have a whole pizza! :D")
-    } while (sliceCount < totalPizzaSlices)
+        num % 3 == 0 && num % 5 == 0 -> "fizzbuzz"
+        num % 3 == 0 -> "fizz"
+        num % 5 == 0 -> "buzz"
+        else -> "$num"
+    })
 }
